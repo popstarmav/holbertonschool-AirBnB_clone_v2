60b20dda6e3f48ff6937ad91471da864990677d1
popstarmav <cj.johnson2017@gmail.com>
2023-11-14 20:27:26 -0800

    web drame route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB """
     return "Hello HBNB!"
 
-"""sever only starts script if direclty activted"""
+
+""" sever only starts script if direclty activted """
 if __name__ == "__main__":
-    #Network interface
+    # Network interface
     app.run(host="0.0.0.0", port=5000)
